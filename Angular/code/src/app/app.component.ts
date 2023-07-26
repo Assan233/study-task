eec7ec6782a171e45b8d5657c49365bdd1290e5d
Assan233 <719685838@qq.com>
2023-07-26 11:03:53 +0800

    angular demo init

---
+++ Angular/code/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+    // 这里配置的title优先级高于html
+    title = 'Angular Demo';
+}
