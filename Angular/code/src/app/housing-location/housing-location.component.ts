ba984cfbdbbf9e463fc6c41019d50444394993ed
Assan233 <719685838@qq.com>
2023-07-26 18:18:31 +0800

    feat: angular init components data

--- Angular/code/src/app/housing-location/housing-location.component.ts
+++ Angular/code/src/app/housing-location/housing-location.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
-  selector: 'app-housing-location',
-  templateUrl: './housing-location.component.html',
-  styleUrls: ['./housing-location.component.css']
+    selector: 'app-housing-location',
+    templateUrl: './housing-location.component.html',
+    styleUrls: ['./housing-location.component.css']
 })
 export class HousingLocationComponent {
-
+    // 感叹号称为非空断言运算符，它告诉 TypeScript 编译器此属性的值不能是 null 或 undefined。
+    @Input()
+    housingLocation!: HousingLocation;
 }
