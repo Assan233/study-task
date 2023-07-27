47bc322dd8297a046a2983ee28ecdf58722aa370
Assan233 <719685838@qq.com>
2023-07-27 15:01:42 +0800

    feat: 路由配置

--- Angular/code/src/app/app.module.ts
+++ Angular/code/src/app/app.module.ts
@@ -4,19 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { provideRouter } from '@angular/router';
+
 import { HousingLocationComponent } from './housing-location/housing-location.component';
+import { DetailsComponent } from './details/details.component';
+import routeConfig from './routes';
 
 @NgModule({
     declarations: [
         AppComponent,
         HomeComponent,
-        HousingLocationComponent
+        HousingLocationComponent,
+        DetailsComponent
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
     ],
-    providers: [],
+    providers: [
+        provideRouter(routeConfig)
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
