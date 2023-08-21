2d7489227d2313c6fc5189becdd60e2a6821d00d
Assan233 <719685838@qq.com>
2023-08-21 18:47:29 +0800

    feat: mysql 接入

--- Node/nestjs/src/app.module.ts
+++ Node/nestjs/src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './serve/user/user.module';
 import { UserInfoModule } from './serve/user-info/user-info.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+const UserDBModule = TypeOrmModule.forRoot({
+  name: 'user', // 用户标记不同的表
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'assan233',
+  database: 'nestjs_db',
+  entities: [],
+});
 
 @Module({
-  imports: [UserModule, UserInfoModule],
+  imports: [UserModule, UserInfoModule, UserDBModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- Node/nestjs/src/serve/user/user.service.ts
+++ Node/nestjs/src/serve/user/user.service.ts
@@ -1,11 +1,14 @@
 import { User } from './interfaces';
 import { Injectable } from '@nestjs/common';
+import { DataSource } from 'typeorm';
 
 @Injectable()
 export class UserService {
+  constructor(private dataSource: DataSource) {}
   private readonly userList: User[] = [];
 
   getUserList(): User[] {
+    console.log(this.dataSource);
     return this.userList;
   }
 
